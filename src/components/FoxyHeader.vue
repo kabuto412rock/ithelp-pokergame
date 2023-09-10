fb2d0b0b7609c5aa418679cb189b974af8726f04
zongjia <kabuto412rock@gmail.com>
2023-09-10 19:31:34 +0800

    feat: 標題和畫面，但狐狸、預備要放卡牌的位置都跑版

---
+++ src/components/FoxyHeader.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+<template>
+    <header>
+        <div>無撲克遊戲</div>
+        <img src="../assets/imgs/foxy01.jpg" class="foxyHead" />
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: #f0f0f0;
+    display: flex;
+    padding: 1rem;
+    width: 100vw;
+    max-height: 20vh;
+    text-align: left;
+}
+
+header div {
+    font-size: 2.5rem;
+    height: auto;
+}
+
+div img.foxyHead {
+    display: inline-block;
+    width: 5rem;
+    height: 5rem;
+}
+</style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card-board">
+        之後放卡牌的地方...
+    </div>
+</template>
+
+<style scoped>
+.card-board {
+    border: 1px solid #000;
+    width: 90%;
+    padding: 2px;
+}
+</style>
