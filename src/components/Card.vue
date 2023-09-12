c8b6ea2c2314c5d18957048e9b1342817e564a83
zongjia <kabuto412rock@gmail.com>
2023-09-12 16:33:20 +0800

    調整卡背的圖超出框框

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -14,11 +14,11 @@ const isOpenClass = props.isOpen ? '' : 'card-back ';
 <template>
     <div class="card">
         <div class="card-front" :class="numberClass">{{ content }}</div>
-        <div class="card-back">back</div>
+        <div class="card-back"></div>
     </div>
 </template>
 <style scoped>
-@keyframes cube-rotate {
+@keyframes card-rotate {
     0% {
         transform: rotateY(0);
     }
@@ -30,19 +30,19 @@ const isOpenClass = props.isOpen ? '' : 'card-back ';
 
 .card:hover {
     /** 結束後不會恢復原狀 */
-    animation: cube-rotate 0.5s forwards;
+    animation: card-rotate 0.5s forwards;
 }
 
 .card {
     --card-font-size: 1.5rem;
     --card-width: 5rem;
+    --card-height: calc(var(--card-width)*1.5);
     width: var(--card-width);
-    height: calc(var(--card-width)*1.5);
+    height: var(--card-height);
     border: 1px solid black;
     border-radius: 5%;
     background-color: white;
-    padding: 3px;
-    margin-right: 1px;
+    margin-right: 10px;
     font-size: var(--card-font-size);
     transform-style: preserve-3d;
 }
@@ -54,15 +54,24 @@ const isOpenClass = props.isOpen ? '' : 'card-back ';
 }
 
 div.card-front {
+    padding: 3px;
+
     transform: translateZ(0px);
 }
 
 div.card-back {
-    width: 100%;
-    height: 100%;
+    position: absolute;
+    /** 避免背圖因為padding超出框框 */
+    left: 5%;
+    width: 90%;
+    height: 95%;
     /*hide backface*/
     background: url("http://127.0.0.1:5173/src/assets/imgs/foxy01.jpg") no-repeat center;
     backface-visibility: hidden;
+    /** 避免超出背圖超出卡片 */
+    background-size: cover;
+    /* 使用 cover 属性来适应容器 */
+
     /*flip card-back behind card-front*/
     transform: rotateY(180deg);
 
