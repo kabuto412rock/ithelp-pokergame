4415da16b06ad49e1d8f5589b3d0ae5f0dab684a
zongjia <kabuto412rock@gmail.com>
2023-09-14 17:16:46 +0800

    調整遊戲大廳的導覽列，雖然沒變多好看，但是至少不會讓人看到很難受了吧?!

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,41 @@ import { RouterView, RouterLink } from 'vue-router';
 </script>
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/game1">撲克牌連連看</RouterLink>
+    <nav class="navbar">
+      <RouterLink to="/" class="nav-link">Home</RouterLink>
+      <RouterLink to="/game1" class="nav-link">撲克牌連連看</RouterLink>
     </nav>
   </header>
   <RouterView />
 </template>
+<style scoped>
+.navbar {
+  background-color: #333;
+  /* 背景顏色 */
+  padding: 10px 0;
+  /* 上下內邊距 */
+  text-align: center;
+  /* 文字居中 */
+}
+
+.nav-link {
+  color: #fff;
+  /* 文字顏色 */
+  text-decoration: none;
+  /* 移除下劃線 */
+  margin: 0 10px;
+  /* 左右外邊距 */
+  font-size: 18px;
+  /* 字體大小 */
+  transition: color 0.3s;
+  /* 讓顏色轉換平滑 */
+}
+
+/* 滑鼠浮過時的效果 */
+.nav-link:hover {
+  color: #ff4500;
+  /* 滑鼠浮過時的顏色 */
+}
+</style>
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,20 @@
 <script setup>
+import GameBoard from '../components/GameBoard.vue';
 </script>
-
 <template>
   <main>
-    這裡是Home，類似遊戲大廳的地方?!
+    <GameBoard>
+      <h1>這裡是Home，類似遊戲大廳的地方?!</h1>
+      <p>
+        我打算在這裡先畫牌堆的樣子
+      </p>
+
+    </GameBoard>
   </main>
 </template>
+
+<style scoped>
+p {
+  color: white;
+}
+</style>
