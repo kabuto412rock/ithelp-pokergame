1a3cd962ba9adf1019de45f3df6b7f1c2ce25bea
zongjia <kabuto412rock@gmail.com>
2023-09-19 14:35:09 +0800

    實現兩個列表的卡牌拖曳

--- src/views/DragDemo.vue
+++ src/views/DragDemo.vue
@@ -4,18 +4,30 @@ import draggable from 'vuedraggable'
 import { geneateDeck } from "../utils/poker-helper";
 import GameBoard from '../components/GameBoard.vue';
 import Card from '../components/Card.vue';
-const firstCardStack = ref(geneateDeck(20, true));
+const firstCardStack = ref(geneateDeck(26, true));
+const secondCardStack = ref([]);
 </script>
 <template>
     <main>
         <GameBoard>
-            <h4 class="title">Demo1</h4>
-            <draggable :list="firstCardStack" @change="console.log" itemKey="value"
-                style="display: grid; grid-template-columns: repeat(13, 3rem);">
-                <template #item="{ element, index }">
-                    <Card :value="element.value" :isOpen="element.isOpen" />
-                </template>
-            </draggable>
+            <div>
+                <h4 class="title">牌堆1</h4>
+                <draggable :list="firstCardStack" group="pokers" @change="console.log" itemKey="value"
+                    style="display: grid; grid-template-columns: repeat(13, 3rem); background-color: yellow;">
+                    <template #item="{ element, index }">
+                        <Card :value="element.value" :isOpen="element.isOpen" />
+                    </template>
+                </draggable>
+            </div>
+            <div>
+                <h4 class="title">牌堆2</h4>
+                <draggable :list="secondCardStack" group="pokers" @change="console.log" itemKey="value"
+                    style="display: grid; grid-template-columns: repeat(13, 3rem); background-color: yellow;padding: 1px;">
+                    <template #item="{ element, index }">
+                        <Card :value="element.value" :isOpen="element.isOpen" />
+                    </template>
+                </draggable>
+            </div>
         </GameBoard>
     </main>
 </template>
