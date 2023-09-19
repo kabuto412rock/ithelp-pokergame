c4db1a892860317fdfe549c7056ffdd1422f0848
zongjia <kabuto412rock@gmail.com>
2023-09-19 17:57:53 +0800

    限制同個牌堆無法拖曳，但可拖曳到不同牌堆

--- src/views/DragDemo.vue
+++ src/views/DragDemo.vue
@@ -6,14 +6,20 @@ import GameBoard from '../components/GameBoard.vue';
 import Card from '../components/Card.vue';
 const firstCardStack = ref(geneateDeck(26, true));
 const secondCardStack = ref([]);
+
+function limitLocalMove(evt, originalEvent) {
+    // 限制同個牌堆無法拖曳
+    return evt.from !== evt.to;
+}
 </script>
 <template>
     <main>
         <GameBoard>
             <div>
                 <h4 class="title">牌堆1</h4>
-                <draggable :list="firstCardStack" group="pokers" @change="console.log" itemKey="value"
-                    style="display: grid; grid-template-columns: repeat(13, 3rem); background-color: yellow;">
+                <draggable :list="firstCardStack" group="pokers" itemKey="value"
+                    style="display: grid; grid-template-columns: repeat(13, 3rem); background-color: yellow;"
+                    :move="limitLocalMove">
                     <template #item="{ element, index }">
                         <Card :value="element.value" :isOpen="element.isOpen" />
                     </template>
@@ -21,8 +27,9 @@ const secondCardStack = ref([]);
             </div>
             <div>
                 <h4 class="title">牌堆2</h4>
-                <draggable :list="secondCardStack" group="pokers" @change="console.log" itemKey="value"
-                    style="display: grid; grid-template-columns: repeat(13, 3rem); background-color: yellow;padding: 1px;">
+                <draggable :list="secondCardStack" group="pokers" itemKey="value"
+                    style="display: grid; grid-template-columns: repeat(13, 3rem); background-color: yellow;padding: 1px;"
+                    :move="limitLocalMove">
                     <template #item="{ element, index }">
                         <Card :value="element.value" :isOpen="element.isOpen" />
                     </template>
