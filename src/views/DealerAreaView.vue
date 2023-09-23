accecc08b0be1f3e08999a5dc89317a475a209b9
zongjia <kabuto412rock@gmail.com>
2023-09-23 22:17:27 +0800

    將移牌區的樣板替換成`draggable`

--- src/views/DealerAreaView.vue
+++ src/views/DealerAreaView.vue
@@ -1,27 +1,28 @@
 <script setup>
-import { computed, onMounted, ref } from 'vue';
+import draggable from 'vuedraggable'
+import { computed, ref, reactive } from 'vue';
 
 import Card from '../components/Card.vue';
 import GameBoard from '../components/GameBoard.vue';
 import { geneateDeck } from '../utils/poker-helper';
 
 const index = ref(0);
-const deck = ref(geneateDeck(14, true));
+const deck = reactive(geneateDeck(14, true));
 
 function clickCard() {
     index.value++;
-    if (index.value > deck.value.length) {
+    if (index.value > deck.length) {
         index.value = 0;
     }
 }
 /** 玩家可拿取的牌 */
 const canTakeCards = computed(() => {
     let startIndex = index.value < 3 ? 0 : index.value - 3;
-    return deck.value.slice(startIndex, index.value);
+    return deck.slice(startIndex, index.value);
 });
 const deckState = computed(() => {
-    if (index.value === 0 && deck.value.length == 0) return 'empty';
-    if (index.value === deck.value.length) return 'full';
+    if (index.value === 0 && deck.length == 0) return 'empty';
+    if (index.value === deck.length) return 'full';
     return 'normal';
 });
 </script>
@@ -31,11 +32,11 @@ const deckState = computed(() => {
             <div class="text">發牌區</div>
             <div style="display: grid; grid-template-columns: 1.5fr 1fr; gap:3rem; width: fit-content;">
                 <!-- 移牌區 - 左邊水平疊牌最多三張 -->
-                <div style="display: grid; grid-template-columns: repeat(3, 3rem);">
-                    <div v-for="card in canTakeCards" :key="card.value">
-                        <Card :value="card.value" :isOpen="true" />
-                    </div>
-                </div>
+                <draggable :list="canTakeCards" group="pokers" itemKey="value" class="list-group">
+                    <template #item="{ element, index }">
+                        <Card :value="element.value" :isOpen="element.isOpen" />
+                    </template>
+                </draggable>
                 <!-- 發牌堆 -->
                 <div class="card-box">
                     <div class="card" style="visibility: hidden;">
@@ -90,5 +91,10 @@ const deckState = computed(() => {
     background: url("@/assets/imgs/foxy01.jpg") no-repeat center;
     /* 使用 cover 屬性讓背景圖包裹在容器內 */
     background-size: cover;
+}
+
+.list-group {
+    display: grid;
+    grid-template-columns: repeat(3, 3rem);
 }
 </style>
